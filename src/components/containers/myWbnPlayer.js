c3fc2c330f073b41a226c8ad59dd8e432a2d49ef
Danielle Moss <dani.code.camp@gmail.com>
2019-03-29 16:29:39 -0400

    checked on why video isn't saving my place - no solution yet

--- src/components/containers/myWbnPlayer.js
+++ src/components/containers/myWbnPlayer.js
@@ -1,12 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import { ThemeProvider } from 'styled-components';
-import Video from '../Video';
-import Playlist from '../containers/Playlist';
-import StyledWbnPlayer from '../styles/StyledWbnPlayer';
+import React, { useState, useEffect } from "react";
+import { ThemeProvider } from "styled-components";
+import Video from "../Video";
+import Playlist from "../containers/Playlist";
+import StyledWbnPlayer from "../styles/StyledWbnPlayer";
 
 const theme = {
   bgcolor: "#353535",
-  bgcolorItem: "#414141",
+  bgcoloritem: "#414141",
   bgcolorItemActive: "#405c63",
   bgcolorPlayed: "#526d4e",
   border: "none",
@@ -16,7 +16,7 @@ const theme = {
 
 const themeLight = {
   bgcolor: "#fff",
-  bgcolorItem: "#fff",
+  bgcoloritem: "#fff",
   bgcolorItemActive: "#80a7b1",
   bgcolorPlayed: "#7d9979",
   border: "1px solid #353535",
@@ -25,51 +25,51 @@ const themeLight = {
 };
 
 const WbnPlayer = props => {
-
   const videos = JSON.parse(document.querySelector('[name="videos"]').value);
+
   const savedState = JSON.parse(localStorage.getItem(`${videos.playlistId}`));
 
   const [state, setState] = useState({
     videos: savedState ? savedState.videos : videos.playlist,
-    activeVideo: savedState ? savedState.activeVideo : videos.playlist[0],
+    activeVideo: savedState ? savedState.activeVideo : videos.playlist,
     nightMode: savedState ? savedState.nightMode : true,
     playlistId: savedState ? savedState.playlistId : videos.playlistId,
-    autoplay: false,
+    autoplay: false
   });
 
-  useEffect(
-    () => {
-      localStorage.setItem(`${state.playlistId}`, JSON.stringify({ ...state }));
-    },
-    [state]
-  )
+  // Use local storage to persist data
+  useEffect(() => {
+    localStorage.setItem(`${state.playlistId}`, JSON.stringify({ ...state }));
+  }, [state]);
 
-  useEffect(
-    () => {
-      const videoId = props.match.params.activeVideo;
-      if (videoId !== undefined) {
-        const newActiveVideo = state.videos.findIndex(
-          video => video.id === videoId
-        );
-        setState({
-          ...state,
-          activeVideo: state.videos[newActiveVideo],
-          autoplay: props.location.autoplay,
-        });
-      } else {
-        props.history.push({
-          pathname: `/${state.activeVideo.id}`,
-          autoplay: false
-        });
-      }
-    },
-    [props.match.params.activeVideo]
-  )
+  useEffect(() => {
+    const videoId = props.match.params.activeVideo;
+    if (videoId !== undefined) {
+      const newActiveVideo = state.videos.findIndex(
+        video => video.id === videoId
+      );
+      setState({
+        ...state,
+        activeVideo: state.videos[newActiveVideo],
+        autoplay: props.location.autoplay
+      });
+    } else {
+      props.history.push({
+        pathname: `/${state.activeVideo.id}`,
+        autoplay: false
+      });
+    }
+  }, [props.match.params.activeVideo]);
 
+  // Set up toggle for switching to nightmode
   const nightModeCallback = () => {
-    setState({ ...state, nightMode: !state.nightMode });
-  }
+    setState(prevState => ({
+      ...prevState,
+      nightMode: !prevState.nightMode
+    }));
+  };
 
+  // What to do when the video has ended
   const endCallback = () => {
     const videoId = props.match.params.activeVideo;
     const currentVideoIndex = state.videos.findIndex(
@@ -85,22 +85,26 @@ const WbnPlayer = props => {
     });
   };
 
+  // Check to see if the video has played for 10 seconds
   const progressCallback = e => {
-    if(e.playedSeconds > 10 && e.playedSeconds < 11) {
+    if (e.playedSeconds > 10 && e.playedSeconds < 11) {
       const videos = [...state.videos];
       const playedVideo = videos.find(
         video => video.id === state.activeVideo.id
-      )
+      );
       playedVideo.played = true;
 
-      setState(prevState => ({ ...prevState, videos }));
+      setState(prevState => ({
+        ...prevState,
+        videos
+      }));
 
       // setState({
       //   ...state,
-      //   videos: state.videos.map( element => {
+      //   videos: state.videos.map(element => {
       //     return element.id === state.activeVideo.id
-      //     ? { ...element, played: true }
-      //     : element;
+      //       ? { ...element, played: true }
+      //       : element;
       //   })
       // });
     }
@@ -108,7 +112,7 @@ const WbnPlayer = props => {
 
   return (
     <ThemeProvider theme={state.nightMode ? theme : themeLight}>
-      {state.videos !== null ? (
+      {state.video !== null ? (
         <StyledWbnPlayer>
           <Video
             active={state.activeVideo}
