7ce80d03a62df0ee7a1936202c8277587bb948ce
Danielle Moss <dani.code.camp@gmail.com>
2019-03-29 15:22:06 -0400

    set up local storage to persist videos

--- src/components/containers/WbnPlayer.js
+++ src/components/containers/WbnPlayer.js
@@ -27,13 +27,20 @@ const themeLight = {
 const WbnPlayer = props => {
   const videos = JSON.parse(document.querySelector('[name="videos"]').value);
 
+  const savedState = JSON.parse(localStorage.getItem(`${videos.playlistId}`));
+
   const [state, setState] = useState({
-    videos: videos.playlist,
-    activeVideo: videos.playlist[0],
-    nightMode: true,
-    playlistId: videos.playlistId,
+    videos: savedState ? savedState.videos : videos.playlist,
+    activeVideo: savedState ? savedState.activeVideo : videos.playlist[0],
+    nightMode: savedState ? savedState.nightMode : true,
+    playlistId: savedState ? savedState.playlistId : videos.playlistId,
     autoplay: false
   });
+
+  // Use local storage to persist data
+  useEffect(() => {
+    localStorage.setItem(`${state.playlistId}`, JSON.stringify({ ...state }));
+  }, [state]);
 
   useEffect(() => {
     const videoId = props.match.params.activeVideo;
